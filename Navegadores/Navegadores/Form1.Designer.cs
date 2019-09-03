c3edf8a00ea848acb893e3005af1ccc0d6336449
Alumno <Alumno@PC25>
2019-09-03 09:28:29 -0500

    web

--- Navegadores/Navegadores/Form1.Designer.cs
+++ Navegadores/Navegadores/Form1.Designer.cs
@@ -29,15 +29,75 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Ir = new System.Windows.Forms.Button();
+            this.Agregar = new System.Windows.Forms.Button();
+            this.Atras = new System.Windows.Forms.Button();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.Ir);
+            this.panel1.Controls.Add(this.Agregar);
+            this.panel1.Controls.Add(this.Atras);
             this.panel1.Location = new System.Drawing.Point(1, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(796, 69);
             this.panel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(616, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(74, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 24);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // Ir
+            // 
+            this.Ir.Location = new System.Drawing.Point(538, 22);
+            this.Ir.Name = "Ir";
+            this.Ir.Size = new System.Drawing.Size(66, 24);
+            this.Ir.TabIndex = 3;
+            this.Ir.Text = "&Ir";
+            this.Ir.UseVisualStyleBackColor = true;
+            this.Ir.Click += new System.EventHandler(this.Ir_Click);
+            // 
+            // Agregar
+            // 
+            this.Agregar.Location = new System.Drawing.Point(666, 3);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(112, 24);
+            this.Agregar.TabIndex = 4;
+            this.Agregar.Text = "&Agregar a favoritos";
+            this.Agregar.UseVisualStyleBackColor = true;
+            this.Agregar.Click += new System.EventHandler(this.Agregar_Click);
+            // 
+            // Atras
+            // 
+            this.Atras.Location = new System.Drawing.Point(11, 22);
+            this.Atras.Name = "Atras";
+            this.Atras.Size = new System.Drawing.Size(66, 24);
+            this.Atras.TabIndex = 2;
+            this.Atras.Text = "&Atras";
+            this.Atras.UseVisualStyleBackColor = true;
+            this.Atras.Click += new System.EventHandler(this.Atras_Click);
             // 
             // webBrowser1
             // 
@@ -46,6 +106,7 @@
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.Size = new System.Drawing.Size(796, 385);
             this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1_DocumentCompleted);
             // 
             // Form1
             // 
@@ -57,6 +118,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -65,6 +128,11 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Ir;
+        private System.Windows.Forms.Button Agregar;
+        private System.Windows.Forms.Button Atras;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Navegadores/Navegadores/Form1.cs
+++ Navegadores/Navegadores/Form1.cs
@@ -21,5 +21,41 @@ namespace Navegadores
         {
             webBrowser1.Navigate("www.google.com");
         }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(webBrowser1.Url.ToString());
+        }
+
+        private void Atras_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void Ir_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Return)
+                webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(comboBox1.SelectedItem.ToString());
+        }
     }
 }
